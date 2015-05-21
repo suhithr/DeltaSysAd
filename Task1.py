8666e0d54ab8e522a83e0bbe745322ac0ff58c75
suhithr <suhithrajesh@yahoo.com>
2015-05-21 18:50:02 +0530

    Fixed the python script

--- Task1.py
+++ Task1.py
@@ -1,7 +1,8 @@
 import os
 for i in range(1, 101):
 	name = 'folder' + str(i)
-	os.makedir(name + '/')
-	open(name + '.txt', 'a').close()
-	os.chmod(name + '/' + name + '.txt', stat.S_IRWXU)
+	if not os.path.exists(name + '/'):
+		os.makedirs(name + '/')
+	open(name + '/' + name + '.txt', 'a').close()
+	os.chmod(name + '/' + name + '.txt', 0700)
 
